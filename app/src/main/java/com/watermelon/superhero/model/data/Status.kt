5d1b615a67ad610e91f95a9309dc824a6575a933
Ahmed Yusef <ahmed.yusef.1993@gmail.com>
2021-10-03 08:20:48 +0300

    feat: add sealed class

--- app/src/main/java/com/watermelon/superhero/model/data/Status.kt
+++ app/src/main/java/com/watermelon/superhero/model/data/Status.kt
@@ -1,3 +1,14 @@
 package com.watermelon.superhero.model.data
 
-sealed class Status
+/**
+ * get status of requests.
+ * @author     Ahmed Yusef
+ * @return     Unit
+ * */
+
+sealed class Status<out T> {
+    object Loading : Status<Nothing>()
+    data class Fail(val message: String) : Status<Nothing>()
+    data class Success<T>(val data: T) : Status<T>()
+}
+
