3572fc254cb09eeb42b41f9e8ffafec8f4196ea2
Ammar Thaar <amar.at909@gmail.com>
2021-10-03 04:23:58 +0300

    create hero diff util

---
+++ app/src/main/java/com/watermelon/superhero/ui/home/HeroDiffUtil.kt
@@ -0,0 +1,18 @@
+package com.watermelon.superhero.ui.home
+
+import androidx.recyclerview.widget.DiffUtil
+
+class HeroDiffUtil(private val oldList: List<String>, private val newList: List<String>) :
+    DiffUtil.Callback() {
+    override fun getOldListSize() = oldList.size
+
+    override fun getNewListSize() = newList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return true
+    }
+}
