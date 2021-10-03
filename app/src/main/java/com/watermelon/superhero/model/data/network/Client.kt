9acf3bf3b1fb74eecf1eccf30b2249704f484493
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-10-03 07:44:20 +0300

    feat: make api request to get a super hero information

---
+++ app/src/main/java/com/watermelon/superhero/model/data/network/Client.kt
@@ -0,0 +1,26 @@
+package com.watermelon.superhero.model.data.network
+
+import com.google.gson.Gson
+import com.watermelon.superhero.model.data.repository.MainRepository
+import com.watermelon.superhero.model.data.response.Parent
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object Client {
+
+    private val client = OkHttpClient()
+
+    fun getSuperHero() {
+        val url = "https://superheroapi.com/api/3025766657670104/search/${MainRepository.superHeroName}"
+        val request = Request.Builder().url(url).build()
+        val response = client.newCall(request).execute()
+        if (response.isSuccessful) {
+            val result =
+                Gson().fromJson(response.body?.string(), Parent::class.java)
+
+        } else {
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/watermelon/superhero/model/data/repository/MainRepository.kt
@@ -0,0 +1,7 @@
+package com.watermelon.superhero.model.data.repository
+
+object MainRepository {
+
+    var superHeroName = "a"
+
+}
