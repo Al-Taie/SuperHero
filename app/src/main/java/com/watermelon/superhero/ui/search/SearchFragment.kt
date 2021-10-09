ad0a12898e90236560c9f5ed2b1c03352d2209f5
Karrar Mohammed <karrar.m.habeeb@gmail.com>
2021-10-10 02:01:51 +0300

    feat: make clear image in edit text clickable

--- app/src/main/java/com/watermelon/superhero/ui/search/SearchFragment.kt
+++ app/src/main/java/com/watermelon/superhero/ui/search/SearchFragment.kt
@@ -10,7 +10,6 @@ import com.watermelon.superhero.model.data.response.Hero
 import com.watermelon.superhero.presenter.ISearchPresenter
 import com.watermelon.superhero.presenter.SearchPresenter
 import com.watermelon.superhero.ui.base.BaseFragment
-import com.watermelon.superhero.ui.home.HomeFragment
 import com.watermelon.superhero.util.slideVisibility
 import kotlinx.coroutines.FlowPreview
 
@@ -31,7 +30,14 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(), ISearchPresenter {
     override fun callBack() {
         sharedElementEnterTransition = TransitionInflater.from(context)
             .inflateTransition(android.R.transition.move)
+
+        binding.apply {
+            clearTextImage.setOnClickListener {
+                searchBar.text.clear()
+            }
+        }
     }
+
 
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentSearchBinding
         get() = FragmentSearchBinding::inflate
